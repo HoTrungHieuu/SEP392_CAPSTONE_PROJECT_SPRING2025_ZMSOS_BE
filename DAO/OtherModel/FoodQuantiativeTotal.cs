ab8b3cff4e7c6274718e042460b18b513a3cc42c
HoTrungHieuu <hieuhtse162093@fpt.edu.vn>
2025-04-13 21:24:10 +0700

    team food statistic

---
+++ DAO/OtherModel/FoodQuantiativeTotal.cs
@@ -0,0 +1,15 @@
+﻿using DAO.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.OtherModel
+{
+    public class FoodQuantiativeTotal
+    {
+        public FoodView? Food {  get; set; }
+        public double? QuantitativeTotal { get; set; }
+    }
+}
---
+++ DAO/OtherModel/TeamFoodStatistic.cs
@@ -0,0 +1,15 @@
+﻿using DAO.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.OtherModel
+{
+    public class TeamFoodStatistic
+    {
+        public TeamView? Team {  get; set; }
+        public List<FoodQuantiativeTotal>? FoodTotals { get; set; }
+    }
+}
